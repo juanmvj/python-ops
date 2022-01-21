8234808f25feb743525642cf628227e8a25b702c
juanmvj <juanmvj_1996@live.com.mx>
2022-01-21 00:57:36 -0600

    3 days  of code

--- probability.py
+++ probability.py
@@ -4,17 +4,16 @@ from typing import List
 def getHitProbability(R: int, C: int, G: List[List[int]]) -> float:
     
     counter = 0
-    for i in range(len(G)):
-        for j in range(len(G)):
-            if (G[i][j] == 1):
+    for i in range(R):
+        for j in range(C):
+            if(G[i][j] == 1):
                 counter = counter + 1
+            
+            
+        
 
-    return 0.0
+    return (counter/(R*C))
 
 
-getHitProbability(2,3,[[0,0,1],[1,0,1]])
+print(getHitProbability(2,2,[[1,1],[1,1]]))
 
-
-test = [[0,0,1],[1,0,1]]
-
-print(test[0][2])
